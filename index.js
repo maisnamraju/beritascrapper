e759372ca5be8ebbeb05eb9c677b117806595575
maisnamraju@gmail.com <mainamraju@gmail.com>
2015-09-01 21:01:33 +0530

    first push

---
+++ index.js
@@ -0,0 +1,23 @@
+(function() {
+
+"use strict";
+
+var app = require('express');
+var cheerio = require('cheerio');
+var mongoose = require('mongoose'); 
+var request = require('request');
+    app = app();
+
+
+ 
+
+
+
+console.log('Its running');
+
+
+
+app.listen(3000);  
+
+
+})();
---
+++ models /News.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var mongoose = require('mongoose');
+
+var News = mongoose.model('News', { 
+		SiteName : {
+			type: 'String',
+			default: null
+		},
+		Published: {
+			type: 'Date',
+			default: Date.now
+		},
+		Url: {
+			type: 'String',
+			required: true
+		},
+		Title: {
+			type: 'String',
+			required: true
+		},
+		Summary: {
+			type: 'String',
+			required: true
+		},
+		Image: {
+			type: 'String',
+			default: null
+		}
+
+});
+
+module.exports = News;
